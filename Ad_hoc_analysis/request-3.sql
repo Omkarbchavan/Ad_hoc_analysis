80c5bea60548640bb1c34076235dd8660141a599
Omkar Chavan <134577520+Omkarbchavan@users.noreply.github.com>
2023-07-27 10:54:48 +0530

    request-3.sql

--- Ad_hoc_analysis/request-3.sql
+++ Ad_hoc_analysis/request-3.sql
@@ -1,5 +1,4 @@
-
-
+-- Provide a report with all the unique product counts for each segment and sort them in descending order of product counts
 select segment,count(distinct product_code) as product_count
 from dim_product
 group by segment
